df3b04640f6bcdf24d6245d962b67449d47a756f
tsl2075 <lys2075@nate.com>
2025-04-16 14:39:31 +0900

    test

--- job02_crawling_titles.py
+++ job02_crawling_titles.py
@@ -1,9 +1,11 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.chrome.options import Options as ChromeOptions
-from webdriver_manager.chrome import ChromeDriverManager
-import time
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.chrome.service import Service as ChromeService
+# from selenium.webdriver.chrome.options import Options as ChromeOptions
+# from webdriver_manager.chrome import ChromeDriverManager
+# import time
+print('test')
+exit()
 
 options = ChromeOptions()
 
